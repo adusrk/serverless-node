19695bfeecf52dc048ea042319ad85f34fc37aa2
Adnan Haider <Adussrk@gmail.com>
2024-03-21 03:57:08 +0530

    Project setup, env and neon inits

---
+++ src/index.js
@@ -0,0 +1,29 @@
+const serverless = require("serverless-http");
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res, next) => {
+  return res.status(200).json({
+    message: "Hello from root!",
+    DATABASE_URL: process.env.DATABASE_URL ? process.env.DATABASE_URL : 'not here'
+  });
+});
+
+app.get("/path", (req, res, next) => {
+  return res.status(200).json({
+    message: "Hello from path!",
+  });
+});
+
+app.use((req, res, next) => {
+  return res.status(404).json({
+    error: "Not Found",
+  });
+});
+
+//Server-full
+// app.listen(3000, () => {
+//   console.log("Your server is now running");
+// })
+
+module.exports.handler = serverless(app);
