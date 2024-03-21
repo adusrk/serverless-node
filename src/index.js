fefbd392b9d511ec9aea7960878028a27c727d78
Adnan Haider <Adussrk@gmail.com>
2024-03-21 20:28:42 +0530

    "Deployed AWS"

--- src/index.js
+++ src/index.js
@@ -1,22 +1,36 @@
 const serverless = require("serverless-http");
 const express = require("express");
-const { neon, neonConfig} = require('@neondatabase/serverless');
+const { neon, neonConfig } = require("@neondatabase/serverless");
+const AWS = require("aws-sdk");
 
+const AWS_REGION = "ap-southeast-1";
+const STAGE = process.env.STAGE || "prod";
 const app = express();
 
-async function dbClient(){
-  neonConfig.fetchConnectionCache = true
-  const sql = neon(process.env.DATABASE_URL);
-  return sql
-}
+const ssm = new AWS.SSM({ region: AWS_REGION });
+const DATABASE_URL_SSM_PARAM = `/serverless-nodejs-api/${STAGE}/database-url`;
 
+async function dbClient() {
+  const paramStoreData = await ssm
+    .getParameter({
+      Name: DATABASE_URL_SSM_PARAM,
+      WithDecryption: true,
+    })
+    .promise();
+  neonConfig.fetchConnectionCache = true;
+  const sql = neon(paramStoreData.Parameter.Value);
+  return sql;
+}
 
 app.get("/", async (req, res, next) => {
   const sql = await dbClient();
-  const [results] = await sql`select now()`
+  // const [results] = await sql`select now()`
+  const [dbNow] = await sql`select now();`;
+  const delta = (Date.now() - dbNow.now.getTime()) / 1000;
   return res.status(200).json({
     message: "Hello from root!",
-    results: results.now,
+    // results: results.now,
+    delta: delta,
   });
 });
 
